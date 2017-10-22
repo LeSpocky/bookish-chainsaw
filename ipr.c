ba4cc5ac668a34ce4cad747aca8973c337b93ed0
Alexander Dahl <post@lespocky.de>
2017-10-22 19:43:38 +0200

    ipr: Add demo implementation

--- ipr.c
+++ ipr.c
@@ -1,4 +1,30 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void ipr( int i, const char *tpl, ... );
+
 int main( void )
 {
-    return 0;
+    ipr( 1, "foo %i", 42 );
+    ipr( 2, "bar %u", 23, 666 );
+    ipr( 3, "baz %s" );
+
+    return EXIT_SUCCESS;
+}
+
+void ipr( int i, const char *tpl, ... )
+{
+    va_list args;
+
+    assert( tpl );
+
+    (void) printf( "%i: ", i );
+
+    va_start( args, tpl );
+    vprintf( tpl, args );
+    va_end( args );
+
+    (void) printf( "\n" );
 }
